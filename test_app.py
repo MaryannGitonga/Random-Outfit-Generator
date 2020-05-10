baf42d5aa9b91c64227c0189c7084a705aee1c02
MaryannGitonga <naygitonga4@gmail.com>
2020-05-10 15:33:32 +0300

    Add image folders and empty test_app.py

---
+++ test_app.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+
+class TestMyOutfitGenerator(TestCase):
+    def test_create_background(self):
+        self.fail()
+
+    def test_create_buttons(self):
+        self.fail()
+
+    def test_create_photo(self):
+        self.fail()
+
+    def test_update_photo(self):
+        self.fail()
+
+    def test__get_next_item(self):
+        self.fail()
+
+    def test_get_next_top(self):
+        self.fail()
+
+    def test_get_prev_top(self):
+        self.fail()
+
+    def test_get_prev_bottom(self):
+        self.fail()
+
+    def test_get_next_bottom(self):
+        self.fail()
+
+    def test_get_prev_shoe(self):
+        self.fail()
+
+    def test_get_next_shoe(self):
+        self.fail()
+
+    def test_create_outfit(self):
+        self.fail()
